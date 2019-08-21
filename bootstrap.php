81285a4d37b0856b1de5774eaf5bbee394e6930f
Viktor Szépe <viktor@szepe.net>
2019-08-21 20:30:19 +0000

    Work on it some more

--- bootstrap.php
+++ bootstrap.php
@@ -8,3 +8,8 @@ define( 'YOAST_VENDOR_NS_PREFIX', 'YoastSEO_Vendor' );
 define( 'YOAST_VENDOR_DEFINE_PREFIX', 'YOASTSEO_VENDOR__' );
 define( 'YOAST_VENDOR_PREFIX_DIRECTORY', 'vendor_prefixed' );
 define( 'WPSEO_NAMESPACES', true );
+
+define( 'YoastSEO_Vendor\RUCKUSING_BASE', '' );
+
+class_alias( Yoast\WP\Free\Oauth\Client::class, 'WPSEO_MyYoast_Client' );
+class_alias( YoastSEO_Vendor\League\OAuth2\Client\Token\AccessTokenInterface::class, 'WPSEO_MyYoast_AccessToken_Interface' );
