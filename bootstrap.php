27a4a6c5dd0460c7a4b8a66d4bff76b6bcda443e
Viktor Szépe <viktor@szepe.net>
2019-08-21 19:47:38 +0000

    Initial

---
+++ bootstrap.php
@@ -0,0 +1,10 @@
+<?php
+
+define( 'WPSEO_FILE', __FILE__ );
+define( 'WPSEO_VERSION', '11.9' );
+define( 'WPSEO_PATH', WPSEO_FILE );
+define( 'WPSEO_BASENAME', WPSEO_FILE );
+define( 'YOAST_VENDOR_NS_PREFIX', 'YoastSEO_Vendor' );
+define( 'YOAST_VENDOR_DEFINE_PREFIX', 'YOASTSEO_VENDOR__' );
+define( 'YOAST_VENDOR_PREFIX_DIRECTORY', 'vendor_prefixed' );
+define( 'WPSEO_NAMESPACES', true );
